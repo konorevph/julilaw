4fdc0d823ff206e0b8ccc5231e59a0dcfc04d845
ksenya <ksenya05.03@mail.ru>
2024-06-20 21:58:19 +0300

    Контакты

---
+++ internal/components/templates/contacts.php
@@ -0,0 +1,21 @@
+<section class="contacts">
+    <h2 class="center"><?= $heading ?></h2>
+
+    <div class="container">
+        <div class="map">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A76b99bc569732b12bc514d7c0cba71f3c275d7c4d0d1824160873ac2184c9816&amp;width=677&amp;height=436&amp;lang=ru_RU&amp;scroll=true"></script>
+        </div>
+        <ul>
+            <li style="margin-bottom:58px;"><h3 style="background-image: url(/img/icons/logo.svg)">Адвокат Голубкова Юлия</h3></li>
+            <li><h4 style="background-image: url(/img/icons/email.svg)">golubkova_julya@mail.ru</h4></li>
+            <li><h4 style="background-image: url(/img/icons/phone.svg)">+7 911 902-22-32</h4></li>
+            <li><h4 style="background-image: url(/img/icons/time.svg)">пн-пт с 9 до 19</h4></li>
+            <li style="margin-bottom:58px;"><h4 style="background-image: url(/img/icons/address.svg)">Малая Посадская ул., 5</h4></li>
+            <li><a href="/form/" class="border">Записаться на консультацию</a></li>
+        </ul>
+    </div>
+        
+    
+
+    
+</section>
---
+++ internal/components/templates/prices_fiz.php
@@ -0,0 +1,9 @@
+<section class="prices_fiz">
+    <h2 class="left"><?= $heading ?></h2>
+    <h3 class="left"><?= $heading_2 ?></h3>
+    <ul>
+        <li>Услуга</li>
+        <li>Стоимость</li>
+    </ul>
+
+</section>
